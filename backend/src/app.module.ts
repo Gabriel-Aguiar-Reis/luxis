60570b4ccd7e020220ef7ee6ba4751fec7b25baf
Gabriel Aguiar <lugafeagre@gmail.com>
2025-04-21 11:11:34 -0300

    fix: :bug: update app module to use Roles Guard provider

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { AppConfigService } from '@/shared/config/app-config.service'
 import { ConfigModule } from '@/shared/config/config.module'
 import { databaseConfig } from '@/shared/config/database.config'
+import { RolesGuard } from '@/shared/infra/auth/roles.guard'
 import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
 import { TypeOrmModule } from '@nestjs/typeorm'
 @Module({
   imports: [
@@ -14,6 +16,12 @@ import { TypeOrmModule } from '@nestjs/typeorm'
     TypeOrmModule.forFeature([])
   ],
   controllers: [],
-  providers: [AppConfigService]
+  providers: [
+    AppConfigService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard
+    }
+  ]
 })
 export class AppModule {}
