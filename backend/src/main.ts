6fe8c1c25106cb000128807f3f6f07cf9fc68e95
Gabriel Aguiar <lugafeagre@gmail.com>
2025-05-04 15:54:11 -0300

    feat: :sparkles: add global execptions filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core'
-// import { GlobalExceptionFilter } from '@/presentation/filters/http-exceptions.filter'
+import { GlobalExceptionFilter } from '@/shared/infra/filters/http-exceptions.filter'
 import { Logger } from '@nestjs/common'
 import { AppModule } from '@/app.module'
 import { AppConfigService } from '@/shared/config/app-config.service'
@@ -10,14 +10,15 @@ async function bootstrap() {
     bufferLogs: true
   })
 
-  // app.useGlobalFilters(new GlobalExceptionFilter())
+  const config = app.get(AppConfigService)
+  app.useGlobalFilters(new GlobalExceptionFilter(config))
 
   app.useLogger(app.get(PinoLogger))
-  const config = app.get(AppConfigService)
   const port = config.getPort() ?? 3000
   app.enableCors({ origin: '*' })
 
   await app.listen(port)
   Logger.log(`App running on http://localhost:${port}`, 'Bootstrap')
 }
+
 bootstrap()
